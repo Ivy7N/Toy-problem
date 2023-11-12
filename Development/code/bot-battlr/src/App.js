4b39392dcd52f9c1957dd871201314ddf69f9e41
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-11-12 11:03:32 +0300

    completed

---
+++ Development/code/bot-battlr/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      {/* ... other components or content ... */}
+      <a href="#">Learn React</a>
+    </div>
+  );
+};
+
+export default App;
