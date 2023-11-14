3a58067bc2c2535e6f97a637dd9eb05808770d23
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-11-14 10:35:01 +0300

    Completed

--- Development/code/qr-code-generator/src/App.js
+++ Development/code/qr-code-generator/src/App.js
@@ -1,15 +1,39 @@
-
-
+import QRCode from 'qrcode'
+import { useState } from 'react';
 
 function App() {
+  const [url, setUrl] =useState('')
+  const [qrcode, setQRCode] =useState('')
 
 
+  const GenerateQRCode = () => {
+    QRCode.toDataURL(url, {
+      width: 800,
+      margin: 2,
+      color: {
+        dark: "#D1E0FF",
+        light: "#000000ff"
+      }
+    }, (err, url) => {
+      if(err) return console.error(err)
 
-
+      console.log(url)
+      setQRCode(url)
+    })
+  }
   return(
     <div className='App'>
         <h1>QR-Code Generator</h1>
-    </div>
+        <input
+        type="text"
+        placeholder="Enter data"
+        value={url}
+        onChange={(evt) => setUrl(evt.target.value)}/>
+        <button onClick={GenerateQRCode}>Generate</button>
+        {qrcode && <>
+          <img src= {qrcode || "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="}/>
+        </>}
+      </div>
   );
 }
 export default App;
